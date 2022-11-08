7b2e573bbb080a6af6d2ea026c16aee5ec1e25c7
Leonardo Lopes <73510665+CodamaisEJ@users.noreply.github.com>
2022-11-08 16:02:46 -0300

    modify

--- src/pages/control-panel/[username]/Quizzes/App.styles.ts
+++ src/pages/control-panel/[username]/Quizzes/App.styles.ts
@@ -102,15 +102,28 @@ export const Wrapper = styled.div`
     margin: 20px;
   }
 
-  .start, .next {
+  .start{
     cursor: pointer;
     background: linear-gradient(180deg, #ffffff, #ffcc91);
     border: 2px solid #d38558;
     box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
     height: 40px;
-    margin: 20px 0;
+    margin: 25px 0;
     padding: 0 40px;
+    margin-left: 400px;
+  }
+
+  .next {
+    cursor: pointer;
+    background: linear-gradient(180deg, #ffffff, #ffcc91);
+    border: 2px solid #d38558;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    height: 40px;
+    margin: 25px 0;
+    padding: 0 40px;
+    margin-left: 0px;
   }
 
   .start {
